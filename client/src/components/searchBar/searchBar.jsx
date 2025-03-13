988719d3818edf4b01f6ffacef5e79ffa1b5497e
kalpit-5 <kalpitparmar55@gmail.com>
2025-03-13 22:01:50 +0530

    Implemented React-router-dom

--- client/src/components/searchBar/searchBar.jsx
+++ client/src/components/searchBar/searchBar.jsx
@@ -1,29 +1,31 @@
 import { useState } from "react";
 import "./searchBar.scss";
 
-
-const types = ["buy" , "rent"]
+const types = ["buy", "rent"];
 
 function SearchBar() {
+  const [query, setQuery] = useState({
+    type: "buy",
+    location: "",
+    minPrice: 0,
+    maxPrice: 0,
+  });
 
-    const [query , setQuery ] = useState({
-        type:"buy",
-        location:"",
-        minPrice:0,
-        maxPrice:0
-    })
-
-    const switchType = (val) => {
-        setQuery(prev=>({...prev, type: val}))
-    }
+  const switchType = (val) => {
+    setQuery((prev) => ({ ...prev, type: val }));
+  };
   return (
     <div className="searchBar">
       <div className="type">
-        {
-            types.map((type) => (
-                <button key={type} onClick={()=>switchType(type)} className={query.type === type ? "active" : ""}>{type}</button>
-            ))
-        }
+        {types.map((type) => (
+          <button
+            key={type}
+            onClick={() => switchType(type)}
+            className={query.type === type ? "active" : ""}
+          >
+            {type}
+          </button>
+        ))}
       </div>
       <form>
         <input type="text" name="location" placeholder="City Location" />
@@ -42,7 +44,7 @@ function SearchBar() {
           placeholder="Max Price"
         />
         <button>
-            <img src="/search.png" alt=""/>
+          <img src="/search.png" alt="" />
         </button>
       </form>
     </div>
---
+++ client/src/routes/listPage/listPage.jsx
@@ -0,0 +1,20 @@
+import { listData } from "../../lib/dummydata";
+import "./listPage.scss";
+import Filter from "../../components/filter/Filter"
+
+function ListPage() {
+
+    const data = listData;
+
+
+  return <div className="listPage">
+    <div className="listContainer">
+        <div className="wrapper">
+            <Filter/>
+        </div>
+    </div>
+    <div className="mapContainer">Map</div>
+  </div>;
+}
+
+export default ListPage;
